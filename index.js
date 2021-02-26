81ee3080a1a2a56e3578997feb899e4d1b97f223
fdavalo <fdavalo@gmail.com>
2021-02-26 12:18:49 +0100

    all

--- index.js
+++ index.js
@@ -1,100 +1,15 @@
-const request=require('request');
-const JSONStream=require('json-stream');
-const fs=require('fs');
-const process=require('process');
-const websocket=require('websocket');
-const http=require('http');
-const readline=require('readline');
+import process from 'process';
+import {Watch} from './watch.js';
 
 "use strict";
 
-var webSocketsServerPort = 8080;
-var webSocketServer = websocket.server;
-
-var clients = [];
-var data = {};
-
-var server = http.createServer(function(request, response) {});
-var wsServer = null;
-
 process.on('uncaughtException', function (err) {
-  console.log(err);
-})
-
-function dispatch(key) {
-	var message = {"request":"one", "key":key, "value":data[key]};
-	var json = JSON.stringify(message);
-	for (var i=0; i < clients.length; i++) {
-		clients[i].sendUTF(json);
-	}
-}
-
-function produce(key) {
-	if (clients.length>0) dispatch(key);
-}
-
-function close(connection) {
-	var index = -1;
-	for (var i=0; i < clients.length; i++) {
-		if (connection==clients[i]) {
-			index = i;
-			break;
-		}
-	}
-	if (index > 0) clients.splice(index, 1);
-}
-
-function wsHandle(request) {
-	console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-	var connection = request.accept(null, request.origin);
-	var index = clients.push(connection) - 1;
-	connection.on('message', function(message) {
-		if (message.type === 'utf8') {
-			var msg = JSON.parse(message.utf8Data);
-			if (msg.request === 'all') {
-				connection.sendUTF(JSON.stringify({"request":"all", "data":data}));
-			}
-		}
-	});
-    connection.on('error', function(connection) {
-		console.log((new Date()) + " Peer " + connection.remoteAddress + " error.");
-		close(connection);
-	});
-	connection.on('close', function(connection) {
-		console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
-		close(connection);
-	});
-}
-
-function wsStart() {
-	server.listen(webSocketsServerPort, function() {});
-	wsServer = new webSocketServer({httpServer: server});
-	wsServer.on('request', wsHandle);
-}
-
-wsStart();
-
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-	terminal: false
+	console.log(err);
 });
+  
+var resources = {
+	'pods':{'serverUrl':'ws://'+process.env.WATCH_PODS_URL+':80/'},
+	'services':{'serverUrl':'ws://'+process.env.WATCH_SERVICES_URL+':80/'}
+};
 
-rl.on('line', function(line){
-	var arr = line.split(' ',5)
-	var aipl = arr[2].split('.')
-	var ipl = aipl[0]+'.'+aipl[1]+'.'+aipl[2]+'.'+aipl[3]
-	var aipd1 = arr[4].split(':')[0]
-	var flow = ipl+"-"+aipd1;
-	if (! data[flow]) {
-		var aipd = aipd1.split('.')
-		var ipd = aipd[0]+'.'+aipd[1]+'.'+aipd[2]+'.'+aipd[3]
-		var portd = aipd[4]
-		if (! data[flow]) {
-			data[flow]={"ipl":ipl,"ipd":ipd,"portd":portd}
-			produce(flow);
-		}
-	}
-});
-
-
+new Watch(process.env.PORT, resources);
---
+++ watch.js
@@ -0,0 +1,139 @@
+import fs from 'fs';
+import process from 'process';
+import readline from 'readline';
+import {WsServer} from './wsserver.js';
+import {WsClient} from './wsclient.js';
+  
+"use strict";
+
+export class Watch {
+
+    constructor(port, resources) {
+        this.Flows = {};
+        this.RawFlows = {};
+        this.Ips = {
+            'localhost': {'ip':'localhost', 'type':'node', 'name':'localhost'}
+        };
+        this.LocalesIps = ['localhost'];
+        
+        this.wsServer = new WsServer(port, resources);
+        new WsClient(resources, this);
+
+        // tcpdump input
+        var rl = readline.createInterface({
+	        input: process.stdin,
+	        output: process.stdout,
+	        terminal: false
+        });
+
+        rl.on('line', this.handleLine.bind(this));
+    }
+
+    handleLine(line) {
+        var arr = line.split(' ',5);
+        var orip = this.checkIpPort(arr[2]);
+        var destp = this.checkIpPort(arr[4]);
+        var flow = orip['host']['ip']+"->"+destp['host']['ip']+":"+destp['port'];
+        if (! this.RawFlows[flow]) {
+            this.RawFlows[flow]={"ori":orip['host'],"dest":destp['host'],"port":destp['port']};
+        }
+    }        
+    
+    checkIpPort(s) { //8.12.12.12.443(:)
+        var arr = s.split(":");
+        var arr1 = arr[0].split(".");
+	    var ip = arr1[0]+'.'+arr1[1]+'.'+arr1[2]+'.'+arr1[3];
+        var host = {'ip':ip, 'name':ip};
+        if (this.Ips[ip]) host = this.Ips[ip];
+        var port = arr1[4];
+        return {'host':host, 'port': port};
+    }
+
+    updateIp(ip) {
+        for (var flow in this.RawFlows) {
+            var updated = false;
+            if ((this.RawFlows[flow]['ori']['ip'] == ip) && (this.RawFlows[flow]['ori']['name'] == ip)) {
+                this.RawFlows[flow]['ori'] = this.Ips[ip];
+                updated = true;
+            }
+            if ((this.RawFlows[flow]['dest']['ip'] == ip) && (this.RawFlows[flow]['dest']['name'] == ip)) {
+                this.RawFlows[flow]['dest'] = this.Ips[ip];
+                updated = true;
+            }
+            if (updated && (this.RawFlows[flow]['ori']['name'] != ip) && (this.RawFlows[flow]['dest']['name'] != ip)) {
+                var flowname = this.RawFlows[flow]['ori']['name']+"->"+this.RawFlows[flow]['dest']['name']+":"+this.RawFlows[flow]['port'];
+                this.Flows[flowname] = this.RawFlows[flow];
+                this.wsServer.produce(flowname, this.Flows);
+            }
+        }
+    }
+
+    checkServiceIP(svc) {
+	    var ip = svc.spec.clusterIP;
+	    if (svc.metadata && svc.spec && ip && (ip != 'None')) {
+            var name = svc.metadata.namespace+".service."+svc.metadata.name;
+		    if (! this.Ips[ip]) {
+			    this.Ips[ip] = {
+				    'ip':ip,
+				    'type':'service',
+				    'namespace':svc.metadata.namespace,
+				    'service':svc.metadata.name,
+				    'name':name
+			    };
+		    }
+		    else if (this.Ips[ip]['name'] != name) {
+			    this.Ips[ip]['type'] = 'service';
+			    this.Ips[ip]['namespace'] = svc.metadata.namespace;
+			    this.Ips[ip]['service'] = svc.metadata.name;
+			    this.Ips[ip]['name'] = name;
+                this.updateIp(ip);
+            }
+	    }
+    }
+
+    checkPodIP(pod) {
+	    var ip = pod.status.podIP;
+	    if ((pod.status.phase=='Running') &&
+		    ((!pod['eventType']) || (pod.eventType != 'DELETED'))) {
+		    if (pod.status.podIP == pod.status.hostIP) {
+			    for (var i in this.LocalesIps) {
+				    var ipl = this.LocalesIps[i];
+                    if (this.Ips[ipl]['name'] != pod.spec.nodeName) {
+				        this.Ips[ipl]['name'] = pod.spec.nodeName;
+				        this.Ips[ipl]['type'] = 'node';
+                        this.updateIp(ipl);
+                    }
+			    }
+			    for (var pid in this.Pids) {
+				    if (this.Pids[pid]['type'] == "node") {
+					    this.Pids[pid]['name'] = pod.spec.nodeName + ".process." + this.Pids[pid]['process'];
+				    }
+			    }
+		    }
+		    else {
+                var name = pod.metadata.namespace+".pod."+pod.metadata.name;
+			    if (! this.Ips[ip]) {
+				    this.Ips[ip] = {
+					    'ip':ip,
+					    'type':'pod',
+					    'namespace':pod.metadata.namespace,
+					    'pod':pod.metadata.name,
+					    'name':name
+				    };
+			    }
+			    else (this.Ips[ip]['name'] != name) {
+				    this.Ips[ip]['type'] = 'pod';
+				    this.Ips[ip]['namespace'] = pod.metadata.namespace;
+				    this.Ips[ip]['pod'] = pod.metadata.name;
+				    this.Ips[ip]['name'] = name;
+                    this.updateIp(ip);
+			    }
+		    }
+	    }
+	    //else if (Ips[ip] && (Ips[ip]['name'] === pod.metadata.namespace+".pod."+pod.metadata.name)) {
+	    //	delete Ips[ip];
+	    //}
+    }
+}
+
+
